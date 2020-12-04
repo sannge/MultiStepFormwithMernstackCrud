83cd5be74d281b1aaf6eacffaf29027f086c1396
San Nge <samngestep@gmail.com>
2020-12-03 22:47:36 -0800

    fixed rightTick

--- client/src/components/FormComponents/Success.js
+++ client/src/components/FormComponents/Success.js
@@ -7,7 +7,7 @@ function Success({onClick}) {
         <div className="mt-5 w-full h-full flex justify-center items-center">
            <div className="md:p-12 sm:shadow-lg w-full md:w-3/4 lg:w-2/4 flex-col flex m-auto">
                <div className="flex justify-center">
-                  <img className="w-24" src={RightTick} alt=""/>
+                  <img className="w-20 object-contain" src={RightTick} alt=""/>
                </div>
                
                <h1 className="mb-5 text-center font-bold text-3xl">You have successfully added an Athlete!</h1>
